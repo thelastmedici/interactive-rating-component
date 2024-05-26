04d4170d12c3f539e585bd86f70554bda43aaae4
thelastmedici <christianopeyemijoshua@gmail.com>
2024-05-26 16:37:53 +0100

    finished js functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const submitButton = document.querySelector(".submit")
 const rating = document.querySelector(".rating")
 const thanks = document.querySelector(".thanks")
 thanks.classList.add('active');
+const innerText = document.querySelector(".inner-text")
 rateButtons.forEach(function(button, i){
     button.addEventListener("click", function(){
         rateButtons.forEach(function (btn) {
@@ -11,10 +12,14 @@ rateButtons.forEach(function(button, i){
         button.classList.add('clicked');
     });
 })
-submitButton.addEventListener("click", function(event){  
+submitButton.addEventListener("click", function(event){
+    const selectedRating = document.querySelector('.clicked');
+    if (selectedRating) {
+        innerText.textContent = `You rated us ${selectedRating.textContent} out of 5!`
+    } else {
+        innerText.textContent = `Please select a rating!`
+    }
      rating.classList.add('active');
-
-    console.log("i got clicked")
     if (rating.classList.contains('active')) {
         thanks.classList.remove('active');
     }
